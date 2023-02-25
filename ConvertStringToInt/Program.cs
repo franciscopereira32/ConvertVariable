fdc53cfe008d7cbaab9615d4a295d169ce32aee2
franciscopereira32 <80694995+franciscopereira32@users.noreply.github.com>
2023-02-25 16:37:06 +1030

    Convert strint to int

---
+++ ConvertStringToInt/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertStringToInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = "1234";
+            int b = Convert.ToInt32(number);
+            Console.WriteLine(b);
+        }
+    }
+}
